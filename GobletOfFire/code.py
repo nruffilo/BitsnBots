5b3d2d29bd3b45ce5147601c7995d69476599259
nruffilo <nickruffilo@gmail.com>
2021-09-29 09:04:47 -0400

    Update code.py

--- GobletOfFire/code.py
+++ GobletOfFire/code.py
@@ -26,42 +26,47 @@ wave = audiocore.WaveFile(wave_file)
 pixels.fill((0,0,0))
 
 flameColor = 1
-flameValue = 20
-flameDirection = 1
+flameValue = 0
+flameArray = [(100,0,0),(100,0,0),(100,0,0),(100,0,0),(100,0,0),(100,0,0),(100,0,0)]
 
 def drawFlame(fv):
-    for i in range(pixelCount):
-        if (flameColor == 1):
-            #pixels[i] = redFlameGradient[random.randint(0,7)]
-            pixels[i] = (
-                min(255, (i+fv)*10),
-                min(255,i+fv*2),
-                min(255,i+fv*2)
-                )
-        else:
-            #pixels[i] = blueFlameGradient[random.randint(0,7)]
-            #pixels[i] = redFlameGradient[random.randint(0,7)]
-            pixels[i] = (
-                min(255, (i+fv)*2),
-                min(255,i+fv*2),
-                min(255,i+fv*8)
-                )
-        pixels.show()
+    #for i in range(pixelCount):
+    if (flameColor == 1):
+        small = fv/8
+        big = fv*3
+        pixels[0] = (max((100 + big)%255,75), small, small)
+        pixels[1] = (max((50 + big)%255,75), small, small)
+        pixels[2] = (max((120 + big)%255,75), small, small)
+        pixels[3] = (max((70 + big)%255,75), small, small)
+        pixels[4] = (max((140 + big)%255,75), small, small)
+        pixels[5] = (max((20 + big)%255,75), small, small)
+        pixels[6] = (max((200 + big)%255,75), small, small)
+
+        #pixels[i] = redFlameGradient[random.randint(0,7)]
+    else:
+        #pixels[i] = blueFlameGradient[random.randint(0,7)]
+        #pixels[i] = redFlameGradient[random.randint(0,7)]
+        pixels[i] = (
+            min(255, (i+fv)*2),
+            min(255,i+fv*2),
+            min(255,i+fv*8)
+            )
+    pixels.show()
 
 
 while True:
     print("Checking HID...")
     print(HID.value)
 
-    if HID.value == True and flameColor == 1:
-        flameColor = 2
-    elif HID.value == False and flameColor == 2:
-        flameColor = 1
+    #if HID.value == True and flameColor == 1:
+    #    flameColor = 2
+    #elif HID.value == False and flameColor == 2:
+    #    flameColor = 1
 
     drawFlame(flameValue)
-    flameValue = flameValue + flameDirection
-    if flameValue > 80 or flameValue < 20:
-        flameDirection = -flameDirection
+    flameValue = flameValue + 1
+    if flameValue > 200:
+        flameValue = 0
     #audio.play(wave)
     #while audio.playing:
     #    pass
