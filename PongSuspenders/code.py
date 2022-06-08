20aea631c9687ad905fe6d629c3aa41e47edf5f2
nruffilo <nickruffilo@gmail.com>
2022-06-08 14:59:04 -0400

    Update code.py

--- PongSuspenders/code.py
+++ PongSuspenders/code.py
@@ -30,6 +30,11 @@ rightLED.value = False
 pongGameActive = 1
 pongGameWinner = 0
 
+timeItActive = True
+timeItWinner = 0
+gameGrid = [[],[]]
+timeItDrops = 0
+
 mode = 1
 gameMode = 0
 gameActive = False
@@ -41,10 +46,12 @@ globalBrightness = .05
 leftPixels = neopixel.NeoPixel(board.A1, pixelCount, brightness=globalBrightness, auto_write=False)
 rightPixels = neopixel.NeoPixel(board.A6, pixelCount, brightness=globalBrightness, auto_write=False)
 cp.pixels.brightness = globalBrightness
+cp.pixels.auto_write = False
 
 ORDER = neopixel.RGB
 ballPosition = 0
 ballDirection = 1
+
 
 
 def wheel(pos):
@@ -73,15 +80,15 @@ def stars():
     leftPixels.fill((0, 0, 0))
     rightPixels.fill((0, 0, 0))
     star = random.randint(-pixelCount+1, pixelCount-1)
-    
+
     if star > 0:
         leftPixels[star] = (255, 255, 255)
-    else: 
+    else:
         rightPixels[-star] = (255, 255, 255)
-    
+
     leftPixels.show()
     rightPixels.show()
-    
+
 
 def rainbow_cycle(wait):
     for j in range(255):
@@ -209,6 +216,106 @@ def pingPong():
         leftPixels.show()
         time.sleep(pongSpeed)
 
+#time it is a simple game where pixels fall from both sides and each side has to press it before it hits the bottom
+def timeIt():
+    global timeItActive, timeItWinner
+    if not timeItActive:
+        if timeItWinner == 1:
+            leftPixels.fill((200, 0, 0))
+            rightPixels.fill((0, 200, 0))
+        else:
+            leftPixels.fill((0, 200, 0))
+            rightPixels.fill((200, 0, 0))
+        leftPixels.show()
+        rightPixels.show()
+        time.sleep(0.3)
+    else:
+        leftPixels.fill((0, 0, 0))
+        rightPixels.fill((0, 0, 0))
+        global gameGrid, timeItDrops, blueLives, redLives
+        global leftButtonPressed, rightButtonPressed, leftButtonPrevious, rightButtonPrevious
+
+        leftButtonCurrent = leftButton.value
+        rightButtonCurrent = rightButton.value
+
+        leftOffset = 0
+        rightOffset = 0
+        if not leftButtonCurrent and leftButtonPrevious:
+            leftButtonPressed = True
+            leftOffset = 155
+        else:
+            leftButtonPressed = False
+
+        if not rightButtonCurrent and rightButtonPrevious:
+            rightButtonPressed = True
+            rightOffset = 155
+        else:
+            rightButtonPressed = False
+
+        leftButtonPrevious = leftButtonCurrent
+        rightButtonPrevious = rightButtonCurrent
+
+        leftPixels[70] = (100 + leftOffset, 0, 0)
+        leftPixels[69] = (100 + leftOffset, 0, 0)
+        leftPixels[68] = (100 + leftOffset, 0, 0)
+        leftPixels[67] = (100 + leftOffset, 0, 0)
+        rightPixels[70] = (0, 0, 100 + rightOffset)
+        rightPixels[69] = (0, 0, 100 + rightOffset)
+        rightPixels[68] = (0, 0, 100 + rightOffset)
+        rightPixels[67] = (0, 0, 100 + rightOffset)
+
+        addBall = random.randint(0,100-timeItDrops)
+        if addBall == 0:
+            gameGrid[0].append(0)
+            gameGrid[1].append(0)
+
+        #move all the balls down
+        for i in gameGrid[0]:
+            gameGrid[0][i] = gameGrid[0][i] + 1
+            gameGrid[1][i] = gameGrid[1][i] + 1
+            
+            # check to to see if it's beyond the bottom
+            if gameGrid[0][i] >= pixelCount:
+                gameGrid[0][i] = 0
+                blueLives = blueLives - 1
+                rightPixels.fill((200,0,0))
+            
+            if gameGrid[1][i] >= pixelCount:
+                gameGrid[1][i] = 0
+                redLives = redLives - 1
+                leftPixels.fill((200, 0, 0))
+                           
+            leftPixels[gameGrid[0][i]] = (255, 255, 255)
+            rightPixels[gameGrid[0][i]] = (255, 255, 255)
+        
+        #check to see if button was pressed and there was a pixel in there, if so, push it to top, otherwise lose a life
+        if leftButtonPressed:
+            foundBall = False
+            for i in gameGrid[1]:
+                if gameGrid[1][i] >= 67:
+                    foundBall = True
+                    gameGrid[1][i] = 0
+            
+            if not foundBall:
+                redLives = redLives - 1
+                leftPixels.fill((200, 0, 0))
+                
+        if rightButtonPressed:
+            foundBall = False
+            for i in gameGrid[0]:
+                if gameGrid[0][i] >= 67:
+                    foundBall = True
+                    gameGrid[0][i] = 0
+            
+            if not foundBall:
+                blueLives = blueLives - 1
+                rightPixels.fill((200, 0, 0))
+        
+        leftPixels.show()
+        rightPixels.show()
+
+        time.sleep(.05)
+
 
 modeButtonPressed = False
 pongSpeed = 0.08
@@ -244,37 +351,47 @@ while True:
     if mode == 1:
         stars()
         time.sleep(.1)
-    
+
 #    if mode == 2:
 #        rainbow_cycle()
 #        time.sleep(.1)
-        
+
     # game select
-    if mode == 2:        
-        if not gameActive: 
+    if mode == 2:
+        if not gameActive:
             cp.pixels.fill((0, 0, 0))
             if gameMode == 0:
-                cp.pixels[3]((0, 0, 200))
-                cp.pixels[4]((0, 0, 200))
+                cp.pixels[3] = (0, 0, 200)
+                cp.pixels[4] = (0, 0, 200)
             elif gameMode == 1:
-                cp.pixels[1]((0, 0, 200))
-                cp.pixels[0]((0, 0, 200))
+                cp.pixels[1] = (0, 200, 200)
+                cp.pixels[0] = (0, 200, 200)
             elif gameMode == 2:
-                cp.pixels[9]((0, 0, 200))
-                cp.pixels[8]((0, 0, 200))
+                cp.pixels[9] = (200, 0, 200)
+                cp.pixels[8] = (200, 0, 200)
             elif gameMode == 3:
-                cp.pixels[5]((0, 0, 200))
-                cp.pixels[6]((0, 0, 200))
-            
-            if (not leftButton.value or not rightButton.value) and not modeSelectiong:
+                cp.pixels[5] = (200, 200, 0)
+                cp.pixels[6] = (200, 200, 0)
+                
+            cp.pixels.show()
+
+            if (not leftButton.value or not rightButton.value) and not modeSelecting:
                 gameMode = gameMode + 1
                 if gameMode > 3:
                     gameMode = 0
+                timeItActive = True
+                timeItWinner = 0
+                pongGameActive = True
+                pongGameWinner = 0
+                gameGrid = [[],[]]
+                redLives = 3
+                blueLives = 3
+                timeItDrops = 0
                 modeSelecting = True
             elif leftButton.value and rightButton.value and modeSelecting:
                 modeSelecting = False
-            
-            if cp.button_b.value:
+
+            if cp.button_b:
                 gameActive = True
 
             time.sleep(0.1)
@@ -287,8 +404,8 @@ while True:
                 defender()
             if gameMode == 3:
                 beats()
-            
-            
-    
+
+
+
     if mode == 3:
         pingPong()
